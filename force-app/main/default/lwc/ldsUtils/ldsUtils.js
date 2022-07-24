2a08d191f8634fc9ffae99c386562c5991455b9c
victor29121990 <109891550+victor29121990@users.noreply.github.com>
2022-07-24 20:52:50 +0530

    Create ldsUtils.js

---
+++ force-app/main/default/lwc/ldsUtils/ldsUtils.js
@@ -0,0 +1,56 @@
+/**
+ * Reduces one or more LDS errors into a string[] of error messages.
+ * @param {FetchResponse|FetchResponse[]} errors
+ * @return {String[]} Error messages
+ */
+ export function reduceErrors(errors) {
+    if(errors instanceof String)
+    {
+        return errors;
+    }
+    if (!Array.isArray(errors)) {
+        errors = [errors];
+    }
+    
+    return (
+        errors
+            // Remove null/undefined items
+            .filter((error) => !!error)
+            // Extract an error message
+            .map((error) => {
+                // UI API read errors
+                if (Array.isArray(error.body)) {
+                    return error.body.map((e) => e.message);
+                }
+                //fieldErrors from UI API DML
+                else if (
+                    error?.body?.output?.fieldErrors &&
+                    Object.keys(error.body.output.fieldErrors).length > 0
+                ) {
+                    const fieldErrors = [];
+                    Object.values(error.body.output.fieldErrors).forEach(
+                        (errorArray) => {
+                            fieldErrors.push(
+                                ...errorArray.map((e) => e.message)
+                            );
+                        }
+                    );
+                    return fieldErrors;
+                }
+                // UI API DML, Apex and network errors
+                else if (error.body && typeof error.body.message === 'string') {
+                    return error.body.message;
+                }
+                // JS errors
+                else if (typeof error.message === 'string') {
+                    return error.message;
+                }
+                // Unknown error shape so try HTTP status text
+                return error.statusText;
+            })
+            // Flatten
+            .reduce((prev, curr) => prev.concat(curr), [])
+            // Remove empty strings
+            .filter((message) => !!message)
+    );
+}
